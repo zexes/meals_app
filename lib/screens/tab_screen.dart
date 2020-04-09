895ef724a00fc67593badfb279266374e936ff82
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-04-09 21:18:39 +0100

    Add a Bottom TabBar in place of Top Tabbar. closes #16

--- lib/screens/tab_screen.dart
+++ lib/screens/tab_screen.dart
@@ -10,33 +10,52 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    //since dynamic or Object, we can choose to add actions
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+      //'action': FlatButton(onPressed: ,)
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorites',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 1,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage, // flutter automatically binds index
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+//        selectedFontSize: 18.0,
+//        unselectedFontSize: 14.0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          )
+        ],
       ),
     );
   }
